9c6ae11054c20e48d45dd3640a9740a36ab64836
Heather Ward <heather.ward13@gmail.com>
2020-01-02 16:55:00 -0500

    Add basic functionality

---
+++ src/components/Timeline.vue
@@ -0,0 +1,243 @@
+<template>
+  <div>
+    <h1>
+      Hi this is a timeline app
+    </h1>
+    <div>
+      <input v-model="personName" v-on:keyup.enter="lookupPerson" ref="input" />
+      <button @click="lookupPerson">Add it!</button>
+    </div>
+    <div v-if="errorMessage" id="error">
+      {{errorMessage}}
+    </div>
+    <div>
+      <div v-if="possibleMatches">
+        <table>
+          <tr>
+            <td v-for="(match, index) in possibleMatches" v-bind:key="index">
+              <button @click="parseLifespan(match)">{{match.label}}</button>
+            </td>
+          </tr>
+<!--          <tr>-->
+<!--            <td v-for="(match, index) in possibleMatches" v-bind:key="index">-->
+<!--              <img v-bind:src="possibleMatchesImages[index]">-->
+<!--            </td>-->
+<!--          </tr>-->
+          <tr>
+            <td v-for="(match, index) in possibleMatches" v-bind:key="index">
+              {{match.description}}
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(entity, index) in timelineEntities" v-bind:key="index">
+          {{entity.name}}: {{entity.birthDate}} to {{entity.deathDate || 'present'}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timeline",
+
+  data() {
+    return {
+      personName: "",
+      possibleMatches: [],
+      possibleMatchesImages: [],
+      errorMessage: "",
+      timelineEntities: []
+    };
+  },
+
+  methods: {
+    async lookupPerson() {
+      this.errorMessage = "";
+      var url = new URL("http://www.wikidata.org/w/api.php");
+      var params = {
+        origin: '*',
+        action: 'wbsearchentities',
+        language: 'en',
+        format: 'json',
+        search: this.personName
+      };
+      Object.keys(params).forEach(key =>
+              url.searchParams.append(key, params[key])
+      );
+      try {
+        const response = await fetch(url);
+        const data = await response;
+        const searchResults = await data.json();
+        if (this.personName === searchResults.search[0].label) {
+          this.parseLifespan(searchResults.search[0]);
+        } else {
+          this.showOptions(searchResults.search);
+        }
+      } catch (error) {
+        throw Error("Error searching Wikidata");
+      }
+    },
+
+    async parseLifespan(entity) {
+      const entityId = entity.id;
+      var url = new URL("http://www.wikidata.org/w/api.php");
+      var params = {
+        origin: '*',
+        action: 'wbgetclaims',
+        format: 'json',
+        entity: entityId
+      };
+      Object.keys(params).forEach(key =>
+              url.searchParams.append(key, params[key])
+      );
+      try {
+        var birthDate;
+        var deathDate;
+
+        const response = await fetch(url);
+        const data = await response;
+        const entityClaims = await data.json();
+        const claims = entityClaims.claims;
+
+        //  check whether the entity is a human
+        if ('P31' in claims) {
+          var instanceClaims = [];
+          claims.P31.forEach(function(instance) {
+            instanceClaims.push(instance.mainsnak.datavalue.value.id);
+          });
+
+          // human or human character in the Old Testament
+          if (instanceClaims.includes('Q5') || instanceClaims.includes('Q20643955')) {
+            //  if human, retrieve birth and death dates
+            if ('P569' in claims) {
+              // This assumes the first date in the array is the best one, not necessarily true
+              var birth = claims.P569[0].mainsnak.datavalue.value.time;
+              if (birth.substring(0, 1) === '-') {
+                birthDate = birth.substring(1, 5).replace(/^0+/, '').concat(" BC");
+              } else {
+                birthDate = birth.substring(1, 5).replace(/^0+/, '').concat(" AD");
+              }
+            } else {
+              this.errorMessage = "Birth date is confusing me, sorry!";
+            }
+
+            if ('P570' in claims) {
+              var death = claims.P570[0].mainsnak.datavalue.value.time;
+              if (death.substring(0, 1) === '-') {
+                deathDate = death.substring(1, 5).replace(/^0+/, '').concat(" BC");
+              } else {
+                deathDate = death.substring(1, 5).replace(/^0+/, '').concat(" AD");
+              }
+            }
+
+            this.addToTimeline(entity.label, birthDate, deathDate);
+
+          } else {
+            console.log(entity);
+            console.log(claims);
+            this.errorMessage = "Selected entity is non-human";
+          }
+        }
+      } catch (error) {
+        console.error(error);
+        throw Error("Error searching Wikidata");
+      }
+
+      this.clearOptions();
+    },
+    //
+    // async getImage(index, entity) {
+    //   const entityId = entity.id;
+    //   var url = new URL("http://www.wikidata.org/w/api.php");
+    //   var params = {
+    //     origin: '*',
+    //     action: 'wbgetclaims',
+    //     format: 'json',
+    //     property: 'P18',
+    //     entity: entityId
+    //   };
+    //   Object.keys(params).forEach(key =>
+    //           url.searchParams.append(key, params[key])
+    //   );
+    //   try {
+    //     const response = await fetch(url);
+    //     const data = await response;
+    //     const imageClaim = await data.json();
+    //     const claim = imageClaim.claims;
+    //
+    //     var urlBase = `https://commons.wikimedia.org/wiki/File:`
+    //     var imgUrl;
+    //     if ('P18' in claim) {
+    //       var file = claim.P18[0].mainsnak.datavalue.value.replace(/ /g, "_");
+    //       imgUrl = `${urlBase}${file}`;
+    //     }
+    //
+    //     this.possibleMatchesImages.splice(index, 1, imgUrl);
+    //   } catch (error) {
+    //     console.log(error);
+    //     throw Error("Some issue getting image");
+    //   }
+    // },
+
+    showOptions(searchResults) {
+      const disambigRegex = /disambiguation/;
+      this.possibleMatches = searchResults.filter(result => {
+        return !result.description.match(disambigRegex);
+      }).slice(0,5);
+      // for (var i = 0; i < this.possibleMatches.length; i++) {
+      //   this.getImage(i, this.possibleMatches[i]);
+      // }
+    },
+
+    clearOptions() {
+      this.possibleMatches = [];
+    },
+
+    addToTimeline(name, birthDate, deathDate) {
+      this.timelineEntities.push({name: name, birthDate: birthDate, deathDate: deathDate});
+      this.personName = "";
+      this.$refs.input.focus();
+    },
+  },
+
+  watch: {
+
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+table {
+  padding-top: 1em;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.person-options {
+  display: inline-block;
+}
+
+#error {
+  color: darkred;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <timeline />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Timeline from "@/components/Timeline.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Timeline
   }
 };
 </script>
